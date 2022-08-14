4c51344ecae210688532a917422006ac49715a04
Piton <brava_05@mail.ru>
2022-08-14 10:35:17 +0300

    Commit 4

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -8,7 +8,7 @@ load_dotenv()
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
-
+print(SECRET_KEY)
 DEBUG = True
 
 ALLOWED_HOSTS = []
